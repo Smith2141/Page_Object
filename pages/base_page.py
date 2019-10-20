f5c457ef6e6d172395331bba37314594e2db62c6
smith_mint <berez_max@mail.ru>
2019-10-20 15:48:43 +0300

    Создать класс MainPage & переписать test_main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,3 @@
-from selenium import webdriver
-import time
-
-driver = webdriver.Chrome()
-l1 = r'https://ru.wikibooks.org/wiki'
-l2 = r'https://stepik.org/lesson/24461/step/9?auth=login&unit=6767'
-
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
@@ -13,7 +6,3 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-
-page = BasePage(driver, l1)
-time.sleep(5)
-page.open()
